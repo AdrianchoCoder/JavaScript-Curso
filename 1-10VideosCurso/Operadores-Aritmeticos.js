af02392b4dc3c215ab8b76232becb70c1cfd3ed3
LinuxAJA <linoandresaj@gmail.com>
2025-06-12 22:44:34 -0500

    se creo un nuevo archivo llamado operadores aritmeticos que utiliza javascript

---
+++ 1-10VideosCurso/Operadores-Aritmeticos.js
@@ -0,0 +1,31 @@
+//Operadores Aritmeticos
+// Operadores Aritmeticos
+let a, b, c, d, e, f;
+
+// Suma
+a = 3 + 4;
+console.log(a);
+
+// Resta 
+b = 6 - 2;
+console.log(b);
+
+//  Multiplicacion
+c = a * 2;
+console.log(c);
+
+// Division 
+d = b / 2.5;
+console.log(d);
+console.log(typeof d);
+
+// Modulo (residuo de la division)
+f = 9 % 2;
+console.log(f);
+
+// Potencia (ES2016 - ECMAScript 2016)
+e = 2 ** 3; // 2 * 2 * 2 = 8
+console.log(e);
+
+e = Math.pow(2,3); // Esto de aqui es para realizar una potencia si es que estamos utilizando una version antigua de JavaScript como dice arriba.
+console.log(e);
