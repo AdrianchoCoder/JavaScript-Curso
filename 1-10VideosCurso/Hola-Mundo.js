8096cde7c814b7774b616d806bae9692ca132db3
LinuxAJA <linoandresaj@gmail.com>
2025-06-12 22:19:22 -0500

    se crearon variables

--- 1-10VideosCurso/Hola-Mundo.js
+++ 1-10VideosCurso/Hola-Mundo.js
@@ -1,1 +1,15 @@
-console.log("Hola Mundo") //Sirve para mandar Informacion a la Consola
+console.log("Hola Mundo"); //Sirve para mandar Informacion a la Consola
+
+var miVariable = 15; //Antes se utilizaba "var", para crear variables, sin embargo este ya esta descontinuado.
+console.log(miVariable);
+let miVariable2 = 10; // Para crear variables lo mas recomendado es utilizar "let", ya que este es el que se utiliza hoy en dia.
+console.log(miVariable2);
+
+/*Tipos de Datos que Utiliza JavaScript:
+Number: Almacena Valores nuemricos
+String: Almacena Cadena de caracteres
+Boolean: Almacena un valor Logico
+Null: Es la Ausencia de una referencia de un objeto 
+Undefined: Ausencia de un valor*/
+
+//
