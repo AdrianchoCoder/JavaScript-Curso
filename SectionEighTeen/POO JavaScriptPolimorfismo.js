699f9afa7f246552736a6bd42a7473a761a2c965
LinuxAJA <linoandresaj@gmail.com>
2025-06-16 22:15:01 -0500

    Se ha agregado Informacion, y un nuevo archivo sobre polimorfismo en javascript

--- SectionEighTeen/POO JavaScriptPolimorfismo.js
+++ SectionEighTeen/POO JavaScriptPolimorfismo.js
@@ -1,4 +1,4 @@
-//EJEMPLO DE HERENCIA
+//EJEMPLO DE HERENCIA CON POLIMORFISMO
 class Empleado {
     constructor(nombre, sueldo) {
         this.nombre = nombre;
@@ -19,8 +19,15 @@ class Gerente extends Empleado{
     }
 }
 
+//Funcion Que permite el Polimorfismo entre La Clase Empleado(Padre) y La Clase Gerente(Hija).
+function imprimir(tipo){
+    console.log(tipo.obtenerInformacion());
+}
+
 let empleado1 = new Empleado("Juan", 1000);
-console.log(empleado1.obtenerInformacion())
 
 let gerente1 = new Gerente("Carlos", 5000, "Sistemas");
-console.log(gerente1.obtenerInformacion());
+
+//Aqui se Muestra como se Imprimela Funcion A traves del Polimorfismo
+imprimir(empleado1);
+imprimir(gerente1);
