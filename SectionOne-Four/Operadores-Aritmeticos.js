1d50c857a5f1327e80fbcd60b78f28c401e2cc0c
LinuxAJA <linoandresaj@gmail.com>
2025-06-22 19:02:31 -0500

    Operadores de Asignacion como se puede asignar un valor a una variable

--- SectionOne-Four/Operadores-Aritmeticos.js
+++ SectionOne-Four/Operadores-Aritmeticos.js
@@ -31,3 +31,41 @@ e = Math.pow(2,3); // Esto de aqui es para realizar una potencia si es que estam
 console.log(e);
 
 //Operadores de Asignacion
+
+// Asignacion
+let x = 5;
+console.log(x);
+// Asignacion con Suma
+x += 3; // x = x + 3
+console.log(x);
+// Asignacion con Resta
+x -= 2; // x = x - 2
+console.log(x);
+// Asignacion con Multiplicacion
+x *= 2; // x = x * 2
+console.log(x);
+// Asignacion con Division
+x /= 4; // x = x / 4
+console.log(x);
+// Asignacion con Modulo
+x %= 3; // x = x % 3
+console.log(x);
+// Asignacion con Potencia
+x **= 2; // x = x ** 2
+console.log(x);
+// Asignacion con Concatenacion
+let str = "Hola";
+str += " Mundo"; // str = str + " Mundo"
+console.log(str);
+// Asignacion con Concatenacion de cadenas
+let str2 = "JavaScript";
+str2 += " es genial"; // str2 = str2 + " es genial"
+console.log(str2);
+// Asignacion con Concatenacion de cadenas y numeros
+let str3 = "El resultado es: ";
+str3 += 42; // str3 = str3 + 42
+console.log(str3);
+// Asignacion con Concatenacion de cadenas y booleanos
+let str4 = "El valor es: ";
+str4 += true; // str4 = str4 + true
+console.log(str4);
