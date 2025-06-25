f1c168be4fe6ed16396f8934a4339cfb5c78fdda
LinuxAJA <linoandresaj@gmail.com>
2025-06-24 20:35:09 -0500

    Terminado Ejercicio de Poo sencillo #1

--- SectionEighTeen-PooJs/EjerciciosPOO/Ejercicio1POO.js
+++ SectionEighTeen-PooJs/EjerciciosPOO/Ejercicio1POO.js
@@ -1,5 +1,24 @@
+//Clase Persona
 class Persona{
+    //Constructor con parametros o argumentos
     constructor(nombre,edad){
-        
+
+        //atributos
+        this.nombre = nombre;
+        this.edad = edad;
     }
-}
+
+    //Metodo
+    Saludar() {
+        return `¡Hola ${this.nombre} de edad ${this.edad}!`;
+    }
+};
+
+//Objetos
+
+let persona1 = new Persona("Adrian", 18);
+let persona2 = new Persona("Alejandra", 15);
+
+//Imprimir
+console.log(persona1.Saludar());
+console.log(persona2.Saludar());
