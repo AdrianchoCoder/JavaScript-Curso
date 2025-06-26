0e732a05c41e4b4bee839e670363258176745bd7
LinuxAJA <linoandresaj@gmail.com>
2025-06-25 20:51:17 -0500

    Creado el Archivo For Con Ejemplos mas Una Imagen Sobre su Concepto

---
+++ SectionFive-TenJs/04_EjemploCicloFor.js
@@ -0,0 +1,17 @@
+//Ciclo For
+//Imprimir valores del 0 al 10
+for (let contador = 0; contador <= 10; contador++) {
+    console.log(contador);
+}
+
+//imprimir los Numeros del 1 al 10 de 3 en 3
+let maximo = 10, minimo = -10;
+for (let numero = 1; numero <= maximo; numero += 3) {
+    console.log(numero);
+}
+
+
+//imprimir los Numeros del 1 al -10 de 3 en 3
+for (numero = 1; numero >= -10; numero -= 3){
+    console.log(numero);
+}
