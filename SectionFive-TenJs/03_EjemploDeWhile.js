485b73a5bf07945994ec9d5062fece1a7ffe0356
LinuxAJA <linoandresaj@gmail.com>
2025-06-25 20:26:41 -0500

    Se Creo el Archivo Ciclo While donde estan While y Do While

---
+++ SectionFive-TenJs/03_EjemploDeWhile.js
@@ -0,0 +1,19 @@
+//Ciclo While
+//Ejemplo de un ciclo while que imprime números del 1 al 10
+let numero = 1; // Inicializamos la variable numero en 1
+while (numero <= 10) { // Mientras numero sea menor o igual a 10
+    console.log(numero); // Imprimimos el valor de numero
+    numero++; // Incrementamos numero en 1
+}
+// Este ciclo continuará hasta que numero sea mayor que 10, imprimiendo los números del 1 al 10 en la consola.
+// Puedes cambiar el valor inicial de numero o el límite superior (10) para ver diferentes resultados
+
+//Ciclo Do While
+//Ejemplo de un ciclo do while que imprime números del 1 al 10
+let numeroDoWhile = 1; // Inicializamos la variable numeroDoWhile en 1
+do {
+    console.log(numeroDoWhile); // Imprimimos el valor de numeroDoWhile
+    numeroDoWhile++; // Incrementamos numeroDoWhile en 1
+} while (numeroDoWhile <= 10); // Mientras numeroDoWhile sea menor o igual a 10
+// Este ciclo se ejecutará al menos una vez, incluso si la condición es falsa desde el principio.
+// Puedes cambiar el valor inicial de numeroDoWhile o el límite superior (10) para ver diferentes resultados.
