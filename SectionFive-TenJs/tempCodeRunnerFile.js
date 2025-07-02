450c22eed722b9f9d86ff799aaf81ed1dbd7a971
LinuxAJA <linoandresaj@gmail.com>
2025-07-01 19:39:52 -0500

    cambios

--- SectionFive-TenJs/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let numeros = 1, maximo = 10, sumador = 0;
-while (numero <= maximo){
-    console.log(numeros);
-    numeros++;
-    sumador += numeros;
-    console.log(`La Suma de los Numeros: ${sumador}`);
-}
