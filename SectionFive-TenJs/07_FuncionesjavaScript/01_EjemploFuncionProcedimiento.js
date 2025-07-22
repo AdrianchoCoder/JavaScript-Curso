0b132a0ca137048d5ee4c764dd1954fa824125c4
LinuxAJA <linoandresaj@gmail.com>
2025-07-22 09:28:39 -0500

    Se crearon dos archivos JavaScript sobre Funciones

---
+++ SectionFive-TenJs/07_FuncionesjavaScript/01_EjemploFuncionProcedimiento.js
@@ -0,0 +1,9 @@
+// Procedimiento JavaScript
+// 1. Definir el Procedimiento
+function saludar( /*Parametro - Variable Local*/ mensaje){
+    //Cuerpo de la Funcion
+    console.log(`Mensaje: ${mensaje}`);
+}
+
+// 2. Llamar el Procedimiento
+saludar( /* Dentro de estos parentesis al llamar la funcion o el procedimiento se le debe pasar unos datos o informacion para que se pueda ejecutar correctamente. Por ejemplo: */ 'Hola' /*En este caso el "Hola" es el argumento o informacion que pasamos para la ejecucion de la funcion por medio del parametro.*/);
---
+++ SectionFive-TenJs/07_FuncionesjavaScript/02_EjemploFuncion.js
@@ -0,0 +1,11 @@
+// Funciones JavaScript
+// 1. Declarar Funcion
+function sumar(a, b){
+    let resultadoSuma = a + b;
+    return resultadoSuma;
+}
+
+// 2. Llamar o Invocar a la Funcion
+let argA = 5, argB = 8;
+let resultadoTotal = sumar(argA, argB);
+console.log(resultadoTotal);
