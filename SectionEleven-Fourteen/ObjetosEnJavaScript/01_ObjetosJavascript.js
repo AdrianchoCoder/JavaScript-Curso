7badae7f0e7579628577b71a483fcb7fd89b9d1c
AdrianchoCoder <adriandaguirrej@gmail.com>
2025-07-29 21:29:06 -0500

    Agregue otra forma de crear objetos en javascript

--- SectionEleven-Fourteen/ObjetosEnJavaScript/01_ObjetosJavascript.js
+++ SectionEleven-Fourteen/ObjetosEnJavaScript/01_ObjetosJavascript.js
@@ -21,3 +21,16 @@ console.log(persona["edad"]); // Acceso con notación de corchetes
 
 //Ahora si queremos acceder a todo el objeto, simplemente lo mostramos en consola.
 console.log(persona); // Muestra todo el objeto persona
+
+// Hay Otra Forma de Crear Objetos en JavaScript y es a Traves de " New Object ", de la Siguiente Manera:
+
+let persona2 = new Object(); // Con esta linea estamos creando el objeto por medio de la variable " persona 2 ".
+
+// Agregaremos la informacion que estara dentro del objeto de la siguiente manera:
+
+persona2.nombre = "Adrian";
+persona2.direccion = "Diagonal 59";
+// De esta manera se pueden agregar todos los datos que vayamos a utilizar de las dos formas se pueden crear objetos
+// Lo mas Recomendado es hacerlo de la primera forma.
+
+console.log(persona2);
